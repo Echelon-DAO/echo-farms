717b32b288ebef7dace85e35889bbe470fc90666
Shahzeb Khan <shahzeb8285@gmail.com>
2021-09-02 21:32:09 +0530

    name fixed

--- src/views/Farms/Farms.tsx
+++ src/views/Farms/Farms.tsx
@@ -101,10 +101,11 @@ const Farms: React.FC<FarmsProps> = (farmsProps) => {
         <Heading as="h1" size="lg" color="primary" mb="50px" style={{ textAlign: 'center' }}>
           {tokenMode
             ? TranslateString(10002, 'Stake tokens to earn ')
-            : TranslateString(320, 'Stake LP tokens to earn EGG')}
+            : "Stake LP tokens to earn ECHO"}
+          
         </Heading>
         <Heading as="h2" color="secondary" mb="50px" style={{ textAlign: 'center', color:"#5677ba" }}>
-          {TranslateString(10000, 'Deposit Fee will be used to buyback EGG')}
+        Deposit Fee will be used to buyback ECHO
         </Heading>
         {window.screen.width > 900 && !tokenMode && (
           <Heading style={{ width: '300px', float: 'right', clear: 'left', display: 'inline', marginTop: '-120px' }}>
